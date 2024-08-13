ae3152a331e0a5238ce8fca6e12615c4b46c8c2d
jakobweber <Jakob-Weber94@web.de>
2024-08-13 17:17:49 +0200

    second finish with some questions

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,4 +1,7 @@
 <template>
+  <div class="spinner-container" v-if="loading">
+    <ion-spinner name="crescent"></ion-spinner>
+  </div>
   <div class="location-button">
     <ion-button @click="moveToCurrentLocation">
       <ion-icon :icon="navigateCircle"></ion-icon>
@@ -13,28 +16,40 @@
         layer-type="base"
         name="OpenStreetMap"
       ></l-tile-layer>
+
+    <l-marker  v-if="currentMarker" :lat-lng="currentMarkerCoordinates"> </l-marker>
+    <l-marker  v-if="searchMarker" :lat-lng="searchMarkerCoordinates"> </l-marker>
     </l-map>
   </div>
 </template>
 
 <script lang="ts">
 import "leaflet/dist/leaflet.css";
-import { IonButton, IonIcon, IonSearchbar  } from "@ionic/vue";
-import { LMap, LTileLayer } from "@vue-leaflet/vue-leaflet";
+import { IonButton, IonIcon, IonSearchbar, IonSpinner  } from "@ionic/vue";
+import {LMap, LTileLayer, LMarker} from "@vue-leaflet/vue-leaflet";
 import { Geolocation } from '@capacitor/geolocation';
+import { Preferences } from '@capacitor/preferences';
 import {defineComponent, ref, nextTick} from "vue";
-import {navigateCircle, search} from "ionicons/icons";
+import {navigateCircle, search, man} from "ionicons/icons";
 import { NativeGeocoder } from '@capgo/nativegeocoder';
+
 
 export default defineComponent({
   components: {
     LMap,
     LTileLayer,
+    LMarker,
     IonButton,
     IonIcon,
-    IonSearchbar
+    IonSearchbar,
+    IonSpinner,
   },
   setup() {
+
+    const searchMarker = ref(false)
+    const searchMarkerCoordinates = ref([0, 0])
+    const currentMarker = ref(false)
+    const currentMarkerCoordinates = ref([0, 0])
 
     const searchInput = ref('');
     const minZoom = 3;
@@ -42,28 +57,79 @@ export default defineComponent({
     const center = ref([47.41322, -1.219482]);
     const map = ref(null);
     const renderComponent = ref(true);
+    const loading = ref(false);
 
 
+    const setupView = async () => {
+      // Retrieve the last center and zoom level from the Preferences storage
+      const savedCenter = await Preferences.get({ key: 'center' });
+      const savedZoom = await Preferences.get({ key: 'zoom' });
+
+      // If the saved values exist, use them to initialize the center and zoom refs
+      if (savedCenter.value) {
+        center.value = JSON.parse(savedCenter.value);
+      }
+      if (savedZoom.value) {
+        zoom.value = JSON.parse(savedZoom.value);
+      }
+    };
+
+    // Call the setupView function when the component is created
+    setupView();
+
+    // SetupView Funktioniert noch nicht
+    // Marker in anderer Farbe und Größe
     // Genz Fragen ob es was schöneres als Force rerender gibt
     const forceRender = async () => {
       renderComponent.value = false;
       await nextTick();
       renderComponent.value = true;
-};
+    };
+
     const moveToCurrentLocation = async () => {
+      // Move the map to the current location
+      loading.value = true;
       const currentLocation = await Geolocation.getCurrentPosition();
       center.value = [currentLocation.coords.latitude, currentLocation.coords.longitude];
       zoom.value = 13; // Adjust zoom level as needed
+      currentMarker.value = true;
+      currentMarkerCoordinates.value = [currentLocation.coords.latitude, currentLocation.coords.longitude];
+
+      await Preferences.set({
+        key: 'center',
+        value: JSON.stringify(center.value),
+      });
+      await Preferences.set({
+        key: 'zoom',
+        value: JSON.stringify(zoom.value),
+      });
+
       await forceRender();
+      loading.value = false;
     };
 
     const moveToLocation = async (adressString: string) => {
+      // Move the map to the location specified by the address string
+      loading.value = true;
       const coordinates = await NativeGeocoder.forwardGeocode({
         addressString: adressString
       });
       center.value = [coordinates.addresses[0].latitude, coordinates.addresses[0].longitude];
       zoom.value = 13; // Adjust zoom level as needed
+      searchMarker.value = true;
+      searchMarkerCoordinates.value = [coordinates.addresses[0].latitude, coordinates.addresses[0].longitude];
+
+      await Preferences.set({
+        key: 'center',
+        value: JSON.stringify(center.value),
+      });
+      await Preferences.set({
+        key: 'zoom',
+        value: JSON.stringify(zoom.value),
+      });
+
       await forceRender();
+      loading.value = false;
     };
 
 
@@ -72,12 +138,18 @@ export default defineComponent({
       zoom,
       center,
       map,
+      loading,
       moveToCurrentLocation,
       moveToLocation,
       navigateCircle,
       search,
+      man,
       searchInput,
-      renderComponent
+      renderComponent,
+      searchMarker,
+      searchMarkerCoordinates,
+      currentMarker,
+      currentMarkerCoordinates
     };
   },
 });
@@ -105,6 +177,16 @@ export default defineComponent({
   left: 50%;
   transform: translateX(-50%);
   z-index: 1000;
-  background-color: rgba(255, 255, 255, 0.8); /* Semi-transparent background */
+  background-color: rgba(255, 255, 255, 0);
 }
+
+.spinner-container {
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  z-index: 1001;
+  font-size: 10em;
+}
+
 </style>
